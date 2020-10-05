a657d62182cf8184db2dae3a79c0aec0d7b7c816
Olimarrex <olimarrex@gmail.com>
2020-10-05 22:45:53 +0100

    a

--- Oli/MainMenu/StartButton.gd
+++ Oli/MainMenu/StartButton.gd
@@ -1,8 +1,8 @@
-#extends TextureRect
+extends TextureRect
 #
 #var scene = preload("res://main.tscn");
 #
-#func _gui_input(event):
-#	if event is InputEventMouseButton:
-#		if event.button_index == BUTTON_LEFT and event.pressed:
-#			get_tree().change_scene("res://main.tscn")
+func _gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			get_tree().change_scene("res://main.tscn")
